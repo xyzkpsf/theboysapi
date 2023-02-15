2843ebeff07401011bfd94d7c4b6c1d359cbff48
xyzkpsf <xyzkpsf@gmail.com>
2023-02-14 20:43:31 -0800

    update characterCard style and hover logic

--- client/src/Component/CharacterCard.tsx
+++ client/src/Component/CharacterCard.tsx
@@ -1,57 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import CircleIcon from '@mui/icons-material/Circle';
 import { CharacterCardProps, characterType } from '../Utils/types';
 
-const mapSpices = (species: string[]): JSX.Element[] => {
-  return species.map((s) => {
-    return (
-      <Typography component={'span'} variant="body2">
-        {s}
-      </Typography>
-    );
-  });
-};
+const CharacterCard = ({ data }: CharacterCardProps) => {
+  const [hover, sethover] = useState(false);
 
-const mapStatusAndSpices = (data: characterType): JSX.Element => {
-  const iconColor = data.status === 'Alive' ? '#228B22' : '#FF0000';
+  const mapStatusAndSpices = (data: characterType): JSX.Element => {
+    const iconColor = data.status === 'Alive' ? '#228B22' : '#FF0000';
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          height: '20px'
+        }}
+        onMouseEnter={() => sethover(true)}
+        onMouseLeave={() => sethover(false)}
+      >
+        {!hover && (
+          <>
+            <CircleIcon
+              sx={{
+                color: iconColor,
+                fontSize: '12px',
+                marginRight: '5px'
+              }}
+            />
+            <Typography component={'span'} variant="body2">
+              {data.status}
+            </Typography>
+          </>
+        )}
+        {hover && (
+          <Button
+            sx={{
+              height: '20px'
+            }}
+            variant="outlined"
+            href={data.url}
+            target="_blank"
+          >
+            More Info
+          </Button>
+        )}
+      </Box>
+    );
+  };
+
   return (
-    <Box
+    <Grid
+      item
+      sm={6}
+      md={4}
+      lg={3}
+      key={data.id}
       sx={{
         display: 'flex',
-        flexDirection: 'row',
+        justifyContent: 'center',
         alignItems: 'center'
       }}
     >
-      <CircleIcon
-        sx={{
-          color: iconColor,
-          fontSize: '15px',
-          marginRight: '5px'
-        }}
-      />
-      <Typography component={'span'} variant="body2">
-        {data.status}
-      </Typography>
-    </Box>
-  );
-};
-
-const CharacterCard = ({ data }: CharacterCardProps) => {
-  return (
-    <Grid item sm={6} md={4} lg={3} key={data.id} justifyContent={'center'} alignItems={'center'}>
       <Box
-        bgcolor={'#DDDDDD'}
         sx={{
           width: '230px',
-          height: '220px',
+          height: '230px',
           display: 'flex',
           flexDirection: 'column',
-          borderRadius: '8px',
-          backgroundColor: '#DDDDDD !important',
+          borderRadius: '12px',
+          backgroundColor: '	#3C415C',
           justifyContent: 'center',
           alignItems: 'center'
         }}
@@ -62,7 +84,7 @@ const CharacterCard = ({ data }: CharacterCardProps) => {
             width: '200px',
             height: '150px',
             objectFit: 'cover',
-            borderRadius: '12px'
+            borderRadius: '15px'
           }}
           alt={data.name}
           src={data.image}
@@ -76,13 +98,16 @@ const CharacterCard = ({ data }: CharacterCardProps) => {
             fontSize: '20px'
           }}
         >
-          <Link href={data.url} variant="h6" color="inherit" underline="none" rel="noopener" target="_blank">
+          <Typography
+            component={'span'}
+            variant="h6"
+            sx={{
+              color: '#D3D3D3'
+            }}
+          >
             {data.name}
-          </Link>
+          </Typography>
           {mapStatusAndSpices(data)}
-          {/* <Button variant="contained" href={data.url} target="_blank">
-            More Info
-          </Button> */}
         </Box>
       </Box>
     </Grid>
