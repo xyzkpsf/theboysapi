9e578a5ac1fbb4c0ee4f1d664ebb371b5f77c39a
xyzkpsf <xyzkpsf@gmail.com>
2023-04-11 23:00:36 -0700

    refactor client to nextjs

---
+++ client/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="h-[50px] flex flex-col items-center justify-center">
+      Navbar
+    </div>
+  );
+}
