48543f5ee19b76d1f2b8ab269c4a8faaabd031d0
xyzkpsf <xyzkpsf@gmail.com>
2023-04-13 23:23:10 -0700

    navBar in progress

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -2,8 +2,14 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="h-[50px] flex flex-col items-center justify-center">
-      Navbar
+    <div className="h-[50px] flex flex-row items-center justify-between">
+      <div className="ml-[30px]">icon here</div>
+      <div className="mr-[30px] flex flex-row gap-x-[20px]">
+        <Link href={"/"}>Home</Link>
+        <Link href={"/"}>Docs</Link>
+        <Link href={"/"}>About</Link>
+        <Link href={"/"}>Contact</Link>
+      </div>
     </div>
   );
 }
