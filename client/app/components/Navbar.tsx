9884094158e547fd7de8a4b0a89c0d803fd31b20
xyzkpsf <xyzkpsf@gmail.com>
2023-04-23 11:29:02 -0700

    remove nextjs files

--- client/app/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Navbar() {
-  return (
-    <div className="h-[50px] flex flex-col items-center justify-center">
-      Navbar
-    </div>
-  );
-}
